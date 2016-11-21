ae87f1baf9d84d3124cb300cac924aebdd6eb97e
jimmybnguyen <jimmyn9@uw.edu>
2016-11-21 15:46:11 -0800

    add quantity key value pair to localstorage

--- cart.js
+++ cart.js
@@ -29,6 +29,13 @@ class Cart extends React.Component {
     render() {
         return (
             <div className="container">
+            
+                <Header
+                />
+            
+                <HomeButton
+            
+                />
                 <InCart
                     movies={this.state.movies}
                 />
@@ -36,10 +43,9 @@ class Cart extends React.Component {
         );
     }
 
-    getMovies(id) {
-            var url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + API_KEY + "&language=en-US";
+    getMovies(product) {
+            var url = "https://api.themoviedb.org/3/movie/" + product.id + "?api_key=" + API_KEY + "&language=en-US";
             var movie = {};
-        
             // fetches data as json and pieces apart information that is displayed
             fetch(url)
             .then((response) => {
@@ -48,14 +54,13 @@ class Cart extends React.Component {
                 var id = json.id;
                 var title = json.title;
                 var poster = json.poster_path;
-                console.log(poster);
                 var overview = json.overview;
                 
                 movie = {id: id, title: title, poster: poster, overview: overview};
                 movies.push(movie);
                 
                 console.log(movies);
-                
+                console.log(this.state);
                 this.setState({
                     movies: movies
                 });
