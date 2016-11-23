06c501dff083c2bc7aacf84d48cc228c8bf7a32d
jimmybnguyen <jimmyn9@uw.edu>
2016-11-23 01:00:08 -0800

    fix styling with in-cart.js

--- in-cart.js
+++ in-cart.js
@@ -4,34 +4,6 @@
 class InCart extends React.Component {
     render() {
         return (
-            <ul>
-                {
-                this.props.movies.map((movie) => (
-                    <li key={movie.id + movie.format}>
-                        <div> 
-                            <h2>{movie.title}</h2>
-                            <div>
-                                <img src= {"http://image.tmdb.org/t/p/w154" + movie.poster} />
-                                <p>{movie.overview}</p>
-                                    <p>Quantity:</p>
-                                    <button onClick={(e) => this.update(e, movie.id, "min", movie.format)}>-</button>
-                                    {movie.quantity}
-                                    <button onClick={(e) => this.update(e, movie.id, "plus", movie.format)}>+</button>
-                                <div>
-                                    <button onClick={(e) => this.delete(e, movie.id, movie.format)}>Remove All</button>
-                                </div>
-                                <div>Format: {movie.format} </div>
-                                <p>Movie total price: {movie.totalPrice}</p>
-                            </div>
-                        </div>
-                    </li>
-                ))
-                }
-                <li>
-                    <p className="total">Total price: {this.props.grandTotal}</p>
-                </li>
-            </ul>
-
             <div className="container">
                 <div className="row">
                     <div className="col-sm-6"> 
@@ -45,11 +17,11 @@ class InCart extends React.Component {
                                             <img src= {"http://image.tmdb.org/t/p/w154" + movie.poster} />
                                             <p>{movie.overview}</p>
                                                 <p>Quantity:</p>
-                                                <button onClick={(e) => this.update(e, movie.id, "min")}>-</button>
+                                                <button onClick={(e) => this.update(e, movie.id, "min", movie.format)}>-</button>
                                                 {movie.quantity}
-                                                <button onClick={(e) => this.update(e, movie.id, "plus")}>+</button>
+                                                <button onClick={(e) => this.update(e, movie.id, "plus", movie.format)}>+</button>
                                             <div>
-                                                <button onClick={(e) => this.delete(e, movie.id )}>Remove All</button>
+                                                <button onClick={(e) => this.delete(e, movie.id, movie.format)}>Remove All</button>
                                             </div>
                                             <p>Movie total price: {movie.totalPrice}</p>
                                         </div>
